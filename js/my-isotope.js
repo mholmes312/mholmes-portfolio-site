d80cf1a682d0da265ff019865a1d572b6107c690
Cale Crawford <cale@typeninecreatives.com>
2018-05-02 12:28:32 -0700

    Update isotope javascript

--- js/my-isotope.js
+++ js/my-isotope.js
@@ -1,19 +1,16 @@
-      var $grid = $('.portfolio-items').isotope({
-        itemSelector: '.item',
-        layoutMode: 'fitRows',
-        getSortData: {
-          name: function (element) {
-            return $(element).text();
-          }
-        }
-      });
-      $('.categories li').on("click", function () {
-        var value = $(this).attr('data-name');
-          $grid.isotope({
-            filter: value
-          });
-        $('.categories li').removeClass('active');
-        $(this).addClass('active');
-      });
-     
+$(document).ready(function(){
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    layoutMode: 'fitRows'
+  });
 
+  $('.categories li').on("click", function () {
+    var value = $(this).attr('data-name');
+      $grid.isotope({
+        filter: value
+      });
+    $('.categories li').removeClass('active');
+    $(this).addClass('active');
+  });
+});
